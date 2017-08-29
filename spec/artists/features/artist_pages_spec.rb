dd914227608c1e432586883694d379eb313c2d2b
samuelssnider <sam_redins@yahoo.com>
2017-08-29 14:29:46 -0600

    test complete

--- spec/artists/features/artist_pages_spec.rb
+++ spec/artists/features/artist_pages_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "As a user" do
-	it "When I visit the artists index" do
+	it "When I visit the artists and try to create a new artist" do
+		visit '/artists'
+
+		artist_name       = "Bob Marley"
+    artist_image_path = "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
+
+
+		click on "New Artist"
+		fill_in "artist_name", with: artist_name
+		fill_in "artist_image_path", with: artist_image_path
+		click on "Create Artist"
+
+		expect(page).to have_content artist_name
+		expect(page).to have_css("img[src=\"#{artist_image_path}\"]")
 	end
 
-	it "And I click 'New Artist'" do
-	end
-
-	it "And I fill in the name" do
-	end
-
-	it "And I fill in an image path" do
-	end
-
-	it "And I click on 'Create Artist'" do
-	end
-
-	it "Then I should see the artist name and image on the page" do
-	end
 end

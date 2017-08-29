987f8ca1dee17be16823d404086cdbe2057dabce
samuelssnider <sam_redins@yahoo.com>
2017-08-29 14:24:47 -0600

    start working on the test suite

---
+++ spec/artists/features/artist_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "As a user" do
+	it "When I visit the artists index" do
+	end
+
+	it "And I click 'New Artist'" do
+	end
+
+	it "And I fill in the name" do
+	end
+
+	it "And I fill in an image path" do
+	end
+
+	it "And I click on 'Create Artist'" do
+	end
+
+	it "Then I should see the artist name and image on the page" do
+	end
+end

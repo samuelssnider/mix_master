672f48899ccbb419133b9f2a5417c6e1674f023a
samuelssnider <sam_redins@yahoo.com>
2017-08-29 15:31:54 -0600

    can't tell why this isn't working

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,5 +4,22 @@ class ArtistsController < ApplicationController
 	end
 
 	def new
+		@artist = Artist.new
 	end
+
+	def show
+		byebug
+	end
+
+	def create
+		@artist = Artist.create(artist_params)
+		redirect_to @artist
+	end
+
+	private
+
+	def artist_params
+		params.require(:artist).permit(:name, :image_path)
+	end
+
 end
